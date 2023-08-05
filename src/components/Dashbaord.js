4d299e46bd930eed9dc2373d450e0f930950ead1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-05 10:00:05 +0300

    final commit

--- src/components/Dashbaord.js
+++ src/components/Dashbaord.js
@@ -1,33 +1,70 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import React, { useEffect } from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-export default function Dashbaord() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand  href="#home">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Dashbaord</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+function Dashboard() {
+	const navigate = useNavigate()
+	axios.defaults.withCredentials = true;
+	useEffect(()=>{
+		axios.get('http://localhost:5000/dashboard')
+		.then(res => {
+			if(res.data.Status === "Success") {
+				if(res.data.role === "admin") {
+					navigate('/');
+				} else {
+					const id = res.data.id;
+					navigate('/employeedetail/'+id)
+				}
+			} else {
+				navigate('/start')
+			}
+		})
+	}, [])
+
+	const handleLogout = () => {
+		axios.get('http://localhost:5000/logout')
+		.then(res => {
+			navigate('/start')
+		}).catch(err => console.log(err));
+	}
+	return (
+		<div className="container-fluid">
+			<div className="row flex-nowrap">
+				<div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+					<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+						<a href="/" className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
+							<span className="fs-5 fw-bolder d-none d-sm-inline">Admin Dashboard</span>
+						</a>
+						<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+							<li>
+								<Link to="/" data-bs-toggle="collapse" className="nav-link text-white px-0 align-middle">
+									<i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </Link>
+							</li>
+							<li>
+								<Link to="/employee" className="nav-link px-0 align-middle text-white">
+									<i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Manage Employees</span> </Link>
+							</li>
+							<li>
+								<Link to="profile" className="nav-link px-0 align-middle text-white">
+									<i className="fs-4 bi-person"></i> <span className="ms-1 d-none d-sm-inline">Profile</span></Link>
+							</li>
+							<li onClick={handleLogout}>
+								<a href="#" className="nav-link px-0 align-middle text-white">
+									<i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col p-0 m-0">
+					<div className='p-2 d-flex justify-content-center shadow'>
+						<h4>Employee Management System</h4>						
+					</div>
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default Dashboard
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Employee() {
+function Employee() {
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    axios.get('http://localhost:5000/getEmployee')
+    .then(res => {
+      if(res.data.Status === "Success") {
+        setData(res.data.Result);
+      } else {
+        alert("Error")
+      }
+    })
+    .catch(err => console.log(err));
+  }, [])
+
+  const handleDelete = (id) => {
+    axios.delete('http://localhost:5000/delete/'+id)
+    .then(res => {
+      if(res.data.Status === "Success") {
+        window.location.reload(true);
+      } else {
+        alert("Error")
+      }
+    })
+    .catch(err => console.log(err));
+  }
+
   return (
-    <div>Employee</div>
+    <div className='px-5 py-3'>
+      <div className='d-flex justify-content-center mt-2'>
+        <h3>Employee List</h3>
+      </div>
+      <Link to="/create" className='btn btn-success'>Add Employee</Link>
+      <div className='mt-3'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Salary</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((employee, index) => {
+              return <tr key={index}>
+                  <td>{employee.name}</td>
+                  <td>{
+                    <img src={`http://localhost:5000/images/`+employee.image} alt="" className='employee_image'/>
+                    }</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.address}</td>
+                  <td>{employee.salary}</td>
+                  <td>
+                    <Link to={`/employeeEdit/`+employee.id} className='btn btn-primary btn-sm me-2'>edit</Link>
+                    <button onClick={e => handleDelete(employee.id)} className='btn btn-sm btn-danger'>delete</button>
+                  </td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
+
+export default Employee
