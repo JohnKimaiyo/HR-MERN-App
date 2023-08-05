102d2552bc68b652b13f7c0f227af52a753c96c7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-05 09:43:30 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashbaord from "./components/Dashbaord";
 import Profile from "./components/Profile";
+import Home from "./components/Home";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashbaord />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
--- src/components/Dashbaord.js
+++ src/components/Dashbaord.js
@@ -8,7 +8,7 @@ export default function Dashbaord() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Home</Navbar.Brand>
+        <Navbar.Brand  href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
